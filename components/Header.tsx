5ee9d3c75caa23898239c1b44db627269c9c06e4
huiwu Zhang <pizza6inch@gmail.com>
2025-03-19 17:27:03 +0800

    feat: Add SVG webpack loader and Button component to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,41 +11,87 @@ import {
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 import { Switch } from "@/components/ui/switch";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import PizzaIcon from "@/public/pizza.svg";
+import Image from "next/image";
+
+import {} from "next/font/google";
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
+  // TODO:: 將靜態資料存入json
+  const navigationItems = [
+    {
+      label: "前端學習筆記",
+      href: "/",
+      content: [
+        {
+          label: "效能優化",
+          href: "/",
+        },
+        {
+          label: "資安防護",
+          href: "/",
+        },
+      ],
+    },
+    {
+      label: "職涯攻略",
+      href: "/",
+      content: [
+        {
+          label: "面試準備",
+          href: "/",
+        },
+        {
+          label: "市場調查",
+          href: "/",
+        },
+      ],
+    },
+  ];
 
   return (
-    <header className="flex items-center border-b-2  shadow-white shadow-2xs">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent className="bg-red-500">
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
-            <NavigationMenuContent className="bg-red-500">
-              <NavigationMenuLink>pop</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <header className="flex items-center justify-between border-b-2  shadow-white shadow-2xs h-16 px-10">
+      <div className="flex items-center cursor-pointer gap-2">
+        <Image src={PizzaIcon} alt="" className="" width={30} height={30} />
+        <h1 className="font-bold ">PIZZA筆記</h1>
+      </div>
+      <div className="flex justify-between items-center">
+        {navigationItems.map((item) => (
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem className="">
+                <NavigationMenuTrigger className="cursor-pointer ">{item.label}</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="w-[100px] flex flex-col gap-2 left-3">
+                    {item.content.map((contentItem) => (
+                      <li key={contentItem.label} className="">
+                        <NavigationMenuLink href={contentItem.href} className=" text-start">
+                          {contentItem.label}
+                        </NavigationMenuLink>
+                      </li>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        ))}
+        <Button variant="ghost" className="cursor-pointer">
+          所有文章
+        </Button>
+        <Button variant="ghost" className="cursor-pointer">
+          關於我
+        </Button>
+      </div>
 
-      <Switch className=" cursor-pointer" onClick={() => setTheme(theme === "dark" ? "light" : "dark")} />
+      <Switch className="cursor-pointer" onClick={() => setTheme(theme === "dark" ? "light" : "dark")} />
     </header>
   );
 };
